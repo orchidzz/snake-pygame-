f6f0bb5e92b942905a89db95f9749697fb8bbd3b
orchidzz <65925715+orchidzz@users.noreply.github.com>
2020-06-13 16:56:11 -0700

    Add files via upload

--- main_game.py
+++ main_game.py
@@ -8,8 +8,9 @@ import math
 pygame.init()
 
 #height and width of the screen
-worldx = 1000
-worldy = 700
+worldx = 400
+worldy = 400
+global background
 
 #multiple screens
 startScr = True
@@ -19,16 +20,13 @@ running = True # main loop of game
 
 #grids for the play screen
 grid_list = []
-''' 
-	[0][0][0]...
-	[0][0][0]...
+'''
+	[[0][0][0]...]
+	[[0][0][0]...]
 	...
 '''
 SIDE_LENGTH = 18 #the length of the grid
 MARGIN = 2 #the width of the margin
-
-#pixels to move coord of snake by according to the pressed key
-DIRECTION_LIST = {"K_LEFT": (- MARGIN - SIDE_LENGTH, 0), "K_RIGHT": (MARGIN + SIDE_LENGTH, 0), "K_DOWN": (0, MARGIN + SIDE_LENGTH), "K_UP": (0, -MARGIN - SIDE_LENGTH)}
 	
 #set initial direction of snake to be up/north
 head_direction = "K_UP"
@@ -36,68 +34,118 @@ head_direction = "K_UP"
 clock = pygame.time.Clock()
 
 '''objects'''
-#snake_body = pygame.sprite.Group()
-snake_parts_list = []
+#func to draw the grid for background
+def setUpBoard():
+	background = pygame.display.set_mode([worldx, worldy], pygame.RESIZABLE)
+	background_color = [0,0,0]
+	background.fill(background_color)
+	for row in range(int(worldy/(SIDE_LENGTH+MARGIN))):
+		grid_list.append([])
+		for column in range(int(worldx/(SIDE_LENGTH+MARGIN))):
+			x_coord = (MARGIN + SIDE_LENGTH) * column + MARGIN #x-coord of the grid 
+			y_coord = (MARGIN + SIDE_LENGTH) * row + MARGIN # y-coord of the grid
+			grid_list[row].append([x_coord + int(SIDE_LENGTH/2), y_coord +int(SIDE_LENGTH/2)]) #append the center of the square according to that grid --> needed to spawn the food which is a circle
+			pygame.draw.rect(background, [80,80,80],[x_coord, y_coord,SIDE_LENGTH,SIDE_LENGTH])
+
+snake_parts_list = []	
 class Snake(pygame.sprite.Sprite):
-	def __init__(self):
+	#list of pixels to move coord of snake by according to the pressed key
+	global DIRECTION_LIST
+	DIRECTION_LIST = {"K_LEFT": (- MARGIN - SIDE_LENGTH, 0), "K_RIGHT": (MARGIN + SIDE_LENGTH, 0), "K_DOWN": (0, MARGIN + SIDE_LENGTH), "K_UP": (0, -MARGIN - SIDE_LENGTH)}
+	
+	def __init__(self, num):
 		pygame.sprite.Sprite.__init__(self)
+		self.number = num #id for each snake's part
+		self.rect = 0
+		self.position = 0
+		snake_parts_list.append(self) #add to snake parts list to control its growth
 		
-	def draw(self, backgound, x_coord, y_coord):
+		self.posList = [] #store the part's position coords whenever it moves
+	
+	def draw(self, x_coord, y_coord):
 		self.sprite = pygame.draw.rect(background, [0,255,0], [x_coord,y_coord, SIDE_LENGTH+MARGIN,SIDE_LENGTH+MARGIN])
-		self.rect = pygame.Rect(self.sprite)
-		snake_parts_list.append(self)
+		self.rect = pygame.Rect(x_coord,y_coord, SIDE_LENGTH+MARGIN,SIDE_LENGTH+MARGIN)
+		
 		self.position = [x_coord,y_coord]
 		
 	def move_(self, new_direction):
-		
-		self.rect.move_ip(new_direction)
-		
+		#reset background in order to redraw rect to make it "move"
+		grid_list.clear()
+		setUpBoard()
 		
 		#change position of snake's head when it moves
 		self.position[0] += new_direction[0]
 		self.position[1] += new_direction[1]
 		
-		pygame.display.flip()
+		#redraw rect at new position
+		self.sprite = pygame.draw.rect(background, [0,255,0], [self.position[0],self.position[1], SIDE_LENGTH+MARGIN,SIDE_LENGTH+MARGIN])
+		self.rect = self.sprite
+		
+		#delete unnecessary stored positions except the last two when a new part is added to follow the prev part
+		if len(snake_parts_list)-1 > self.number:
+			del self.posList[0:len(self.posList)-1]
+		
+		position_element = tuple(self.position) #make it a tuple to not change it when appended as self.position changes continuously which causes the position_element stored to change
+		self.posList.append(position_element)
 	
-	def follow(self, old_direction, new_direction, snake_head):
-		if not self.coords() == snake_head.coords():
-			self.move_(DIRECTION_LIST[old_direction])
-
-		self.direction = new_direction
-		self.move_(DIRECTION_LIST[new_direction])
-	#def die(cls):
-		#cls.()
-	#need func to erase the snake when lose
 	def collides_with(self, sprite):
 		return self.rect.collidepoint(sprite)
 	
-	def coords(self, num):
-		return self.position[num] #as a list
+	def coords(self, indexNum):
+		return self.position[indexNum] #as a list [x,y]
+	
+	def crashed_into_its_parts(self):
+		if self.rect.collidelist(snake_parts_list[1:len(snake_parts_list)-1]) == -1:
+			return False
+		else:
+			return True
+
+class SnakeParts(Snake):
+	def __init__(self, num):
+		super().__init__(num)
 		
-FOOD_group = pygame.sprite.GroupSingle()
+	def follow(self, followPos):
+		self.followPosList = followPos
+		pos = self.followPosList[0]
+		self.position = pos #update its own position --> a tuple
+		
+		#delete unnecessary stored positions except the last two when a new part is added to follow the prev part
+		if (len(snake_parts_list)-1) > self.number:
+			del self.posList[0:len(self.posList)-1] #exclusive of the last element
+		
+		#redraw the part at new position
+		self.sprite = pygame.draw.rect(background, [0,255,0], [ pos[0],pos[1], SIDE_LENGTH+MARGIN,SIDE_LENGTH+MARGIN ])
+		self.rect = self.sprite
+		
+		self.posList.append(self.position)	#add the new position to its list
+
 class Food(pygame.sprite.Sprite):
+	global FOOD_group
+	FOOD_group = pygame.sprite.GroupSingle()
+	
 	def __init__(self):
 		pygame.sprite.Sprite.__init__(self)
+		self.position = 0
 		
-	def spawn(self, background, position):
+	def spawn(self, position):
 		self.sprite = pygame.draw.circle(background, [255,255,0], position, 10)
 		FOOD_group.add(self)
-		pygame.display.update()
+		#pygame.display.update()
 		
 		#coords of the upper left corner of grid the target(a circle) is in equals to the center of circle minus radius
 		self.position = position
 		for i in self.position:
 			i = i - SIDE_LENGTH/2
-	def eaten(self, background, color):
-		self.sprite = pygame.draw.circle(backgound, color, self.position, 10)
-		pygame.display.update()
+	def eaten(self):
+		setUpBoard()
+		#pygame.display.update()
 		self.kill()
 	def coords(self):
 		return self.position # as a list
 
 '''instantiate objects'''
 target = Food()
-head = Snake()
+head = Snake(0)
 
 '''main loop'''
 while running:
@@ -105,6 +153,12 @@ while running:
 	while startScr == True:
 		background = pygame.display.set_mode([worldx, worldy], pygame.RESIZABLE)
 		background.fill([0,0,0])
+		FONT = pygame.font.SysFont('Arial', 50)
+		text1 = FONT.render("Start???", True, [255, 255, 255])
+		text2 = FONT.render("click the screen", True, [255, 255, 255])
+		background.blit(text1, (0,250))
+		background.blit(text2, (0,300))
+		pygame.display.update()
 		#when user quits, ends the loop
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
@@ -116,96 +170,109 @@ while running:
 		
 	#play screen
 	while playScr == True:
-		#draw grids for background
-		#remember to change range when change side length/ margin/ or screen width/height values
+		#draw grids for background when game screen starts 
 		#draw once
 		if len(grid_list) == 0:
-			background = pygame.display.set_mode([worldx, worldy], pygame.RESIZABLE)
-			background_color = [0,0,0]
-			background.fill(background_color)
-			for row in range(35):
-				grid_list.append([])
-				for column in range(50):
-					x_coord = (MARGIN + SIDE_LENGTH) * column + MARGIN #x-coord of the grid 
-					y_coord = (MARGIN + SIDE_LENGTH) * row + MARGIN # y-coord of the grid
-					grid_list[row].append([x_coord + int(SIDE_LENGTH/2), y_coord +int(SIDE_LENGTH/2)]) #append the center of the square according to that grid --> needed to spawn the food which is a circle
-					pygame.draw.rect(background, [80,80,80],[x_coord, y_coord,SIDE_LENGTH,SIDE_LENGTH])
-		
-			head.draw(background, 500, 500)
-		
-		
-		head.move_(DIRECTION_LIST[head_direction])
-		
-		
+			setUpBoard()
+			head.draw(int(worldx/2), int(worldy/2))
+			
 		#check events
 		for event in pygame.event.get():
 			#when arrow keys are pressed, move the head of the snake according to direction (if the direction is not the same as the current direction of the snake's head)
 			if event.type == pygame.KEYDOWN:
-				if event.key in DIRECTION_LIST and event.key != head_direction:
-					head.move_(DIRECTION_LIST[event.key])
-					prev_direction = head_direction
-					head_direction = event.key
-				#snake's body parts follow the head
-					for part in snake_parts_list:
-						part.follow(prev_direction, head_direction, head)	
-			
+				if event.key == pygame.K_DOWN and head_direction != "K_UP" and head_direction != "K_DOWN":
+					#prev_direction = head_direction
+					head_direction = "K_DOWN"
+				elif event.key == pygame.K_UP and head_direction != "K_DOWN" and head_direction != "K_UP":
+					#prev_direction = head_direction
+					head_direction = "K_UP"
+				elif event.key == pygame.K_LEFT and head_direction != "K_RIGHT" and head_direction != "K_LEFT":
+					#prev_direction = head_direction
+					head_direction = "K_LEFT"
+				elif event.key == pygame.K_RIGHT and head_direction != "K_LEFT" and head_direction != "K_RIGHT":
+					#prev_direction = head_direction
+					head_direction = "K_RIGHT"
+				#else:
+					#prev_direction = head_direction
+				#direction_list.append(prev_direction)
 			#when user quits, end the loop
 			if event.type == pygame.QUIT:
 				running = False
 				playScr = False
 		
-		#when food disappears, spawn the food at random grid
-		if not FOOD_group.has(target):
-			target.spawn(background, (grid_list[random.randint(0, 34)][random.randint(0, 49)]))
+		#move the head
+		head.move_(DIRECTION_LIST[head_direction])
 		
-		#when snake eats the food, delete the food + snake grows
-		#need fix
-		if head.collides_with(target.coords()):
-			target.eaten(background, background_color) #delete food
-			
-			#grows??
-			'''
-				list append name
-				name.Snake()
-				find direction of next to last part
-				position name part at (next to last part's position - direction)
-				name.follow()
-			'''	
-			new_part = "part" + str(snake_parts_list.length())
-			snake_parts_list.append(x)
-			new_part = Snake()
-			last_part = snake_parts_list[length-1]
-			coords = last_part.coords()
-			last_part_direction = DIRECTION_LIST[last_part.getattr(direction)] 
-			new_part_x = coords[0] - last_part_direction[0]
-			new_part_y = coords[1] - last_part_direction[1]
-			new_part.draw(background, new_part_x, new_part_y)
-			new_part.follow(prev_direction,head_direction, head)
-			
 		#when snake's head goes out of bound, end game & set lose screen
-		if  head.coords(0) >1000 or head.coords(0) < 0 or head.coords(1) > 1000 or head.coords(1) < 0:
+		if  head.coords(0) > worldx or head.coords(0) < -10 or head.coords(1) > worldy or head.coords(1) < -10:
 			loseScr = True
 			playScr = False
-		print("hi")
+			break
+
+		#when snake's head collides with snake's body parts
+		if head.crashed_into_its_parts():
+			loseScr = True
+			playScr = False
+			break
+			
+		#spawn the food
+		if not FOOD_group.has(target):
+			food_position = (grid_list[random.randint(0, int(worldx/(SIDE_LENGTH+MARGIN)-1))][random.randint(0, int(worldy/(SIDE_LENGTH+MARGIN)-1))])
+			target.spawn(food_position)
+		else:
+			target.spawn(food_position)
 		
+		#move the snake parts one by one, excluding the head (index 0)
+		for part in range(1, len(snake_parts_list)):
+			last_part = snake_parts_list[part-1]
+			snake_parts_list[part].follow(getattr(last_part, "posList"))	
+		
+		#when snake eats the food, delete the food + snake grows
+		if head.collides_with(target.coords()):
+			target.eaten() #delete food
+			
+			last_part = snake_parts_list[len(snake_parts_list)-1] #locate the snake's last part in the list
+			new_part = SnakeParts(len(snake_parts_list)) #create new instance
+			#get last position of the last instance/snake part
+			last_part_x = getattr(last_part, "posList")[len(head.posList)-2][0] 
+			last_part_y = getattr(last_part, "posList")[len(head.posList)-2][1]
+			#draw new snake part
+			new_part.draw(last_part_x, last_part_y)
+		
+		#frame update
 		pygame.display.update()
-		clock.tick(2) # num of frames per second
+		clock.tick_busy_loop(5) # num of frames per second
 		
 	#lose screen --> ask for replay
 	while loseScr == True:
 		#need to create replay button
+		background = pygame.display.set_mode([worldx, worldy], pygame.RESIZABLE)
+		background.fill([0,0,0])
+		
+		FONT = pygame.font.SysFont('Arial', 50)
+		text1 = FONT.render("play again???", True, [255, 255, 255])
+		text2 = FONT.render("click the screen", True, [255, 255, 255])
+		background.blit(text1, (0,250))
+		background.blit(text2, (0,300))
+		pygame.display.update()
+		#check events
 		for event in pygame.event.get():
 			if event.type == pygame.MOUSEBUTTONDOWN:
 				startScr = True
 				loseScr = False
-		#when user quits, ends the loop
-			if event.type == pygame.QUIT:
+				#reset everything
+				grid_list.clear()
+				del snake_parts_list[1:len(snake_parts_list)]
+				
+			#when user quits, ends the loop
+			elif event.type == pygame.QUIT:
 				running = False
 				loseScr = False
 				
 			
-#ends the program
+#end the program
 pygame.quit()
 
 
-'''need to make the snake move'''
+'''need to 
+'''
